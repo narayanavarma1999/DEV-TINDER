e4d65f892eb334d3faf0511d559129d014dda9ed
narayanavarma1999 <narayanavarma1999@gmail.com>
2025-07-02 17:45:43 +0530

    created  server

--- dev-tinder-backend/src/server.js
+++ dev-tinder-backend/src/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
 
+/* 
+*  creating an express instance for building application using express function
+*/
 const app = express()
 
-app.listen(3000, () => {
-    console.log(`Server is running`)
+
+app.use('/test', (req, res) => {
+    res.send("Hello World")
+})
+
+
+/* 
+*   app creates an server with listen function with the 
+*   specified port to run the server on port to process the request 
+*/
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port} `)
 })
