546e0df318084da7943433763dfcf25debe8f75b
narayanavarma1999 <narayanavarma1999@gmail.com>
2025-07-16 04:36:42 +0530

    added file paths for proper route handling

--- dev-tinder-frontend/src/App.jsx
+++ dev-tinder-frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter,  Route, Routes } from 'react-router-dom';
-import Feed from './components/Feed';
-import IntroPage from './components/IntroPage';
 import Home from './pages/Home';
 import LoginForm from './pages/LoginForm';
 import RegisterForm from './pages/RegisterForm';
 import store from './utils/appstore/store';
 import ShimmerLoading from './utils/spinner/ShimmerLoadings';
+import IntroPage from './pages/IntroPage';
+import Feed from './components/features/Feed';
 
 export default function App() {
 
@@ -14,11 +14,11 @@ export default function App() {
       <Provider store={store}>
         <BrowserRouter basename='/'>
           <Routes>
-            <Route path="/" element={<IntroPage />} />
+            <Route path="/" element={<IntroPage/>} />
             <Route path="/lazy" element={<ShimmerLoading/>} />
             <Route
               path="/home"
-              element={<Home />}
+              element={<Home/>}
             />
             <Route
               path="/feed"
